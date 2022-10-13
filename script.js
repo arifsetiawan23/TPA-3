dbb282f8a0ef5703a839d8582c79c761bb8a3793
arifsetiawan23 <arifstyawan239@gmail.com>
2022-10-13 19:57:10 +0700

    menambahkan fitur search

--- script.js
+++ script.js
@@ -1,45 +1,29 @@
-const API_URL =  'https://api.themoviedb.org/3/movie/popular?api_key=7144e46790bc896a9a1c877126c190a9&language=en-US&page=1'
-const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?&api_key=0f384af42e76e72036056fa90c1b9d00&query=';
+let MovieList = document.getElementById("movie-list");
+let content = document.querySelector(".content");
+let inputSearch = document.getElementById("search");
+let form = document.getElementById("form");
 
-const main = document.getElementById('main')
-const form = document.getElementById('form')
-const search = document.getElementById('search')
-const content = document.querySelector('.content')
+const URL_API ="https://api.themoviedb.org/3/movie/popular?api_key=7144e46790bc896a9a1c877126c190a9&language=en-US&page=1";
 
-// Get initial movies
-getMovies(API_URL)
+let getDataMovie = async (URL) => {
+  let response = await fetch(URL);
+  let movies = await response.json();
 
-async function getMovies(url) {
-    const res = await fetch(url)
-    const data = await res.json()
-
-    showMovies(data.results)
-}
-
-
-function showMovies(movies) {
-    main.innerHTML = ''
-
-    movies.forEach((movie) => {
-        const { title, poster_path, vote_average, genres, release_date} = movie
-        const movieEl = document.createElement('div')
-        movieEl.classList.add('movie')
-        movieEl.innerHTML = `
-        <div class="p-2">
-          <div class="flex-col card m-auto" style="width: 242px;">
-            <img src="${IMG_PATH + poster_path}" alt="${title}" class="card-img-top">
-            <div class="card-body">
-              <h4 class="card-title">${title}</h4>
-              <p class="card-text">Realese : ${release_date}</p>
-              <h5 class="card-text">Rating : ${vote_average}</h5>
-            </div>
-          </div>
-        </div> 
-        `
-        main.appendChild(movieEl)
-    })
-}
+  movies.results.forEach((item, index) => {
+    let IMG = "https://image.tmdb.org/t/p/w500";
+    MovieList.innerHTML +=
+    `<div class="p-2">
+      <div class="flex-col card m-auto" style="width: 243px;">
+        <img src="${IMG + item.poster_path}" alt="" class="card-img-top">
+        <div class="card-body">
+            <h4 class="card-title">${item.title}</h4>
+            <p class="card-text">Realese : ${item.release_date}</p>
+            <p class="card-text">Rating : ${item.vote_average}</p>
+        </div>
+      </div>
+    </div>`;
+  });
+};
 
 let getDataSearch = async (URL) => {
   let response = await fetch(URL);
@@ -50,14 +34,14 @@ let getDataSearch = async (URL) => {
     let IMG = "https://image.tmdb.org/t/p/w500";
     let htmlSegment = 
     `<div class="p-2">
-    <div class="flex-col card m-auto" style="width: 20rem;">
+      <div class="flex-col card m-auto" style="width: 243px;">
         <img src="${IMG + item.poster_path}" alt="" class="card-img-top">
         <div class="card-body">
             <h3 class="card-title">${item.title}</h3>
             <h5 class="card-text">Realese : ${item.release_date}</h5>
             <h5 class="card-text">Rating : ${item.vote_average}</h5>
         </div>
-    </div>
+      </div>
     </div>`;
 
     html += htmlSegment;
@@ -66,13 +50,14 @@ let getDataSearch = async (URL) => {
   content.innerHTML = html;
 };
 
-showMovies(API_URL);
+getDataMovie(URL_API);
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", (a) => {
   let SEARCH_URL =
     "https://api.themoviedb.org/3/search/movie?api_key=d41688cb0f0ee69b62145a634b120198&query=";
   let searchValue = inputSearch.value;
-  e.preventDefault();
- // desc.innerText = "You searched for " + '"' + searchValue + '"';
+  a.preventDefault();
   getDataSearch(SEARCH_URL + searchValue + "&page=1");
 });
+
+
